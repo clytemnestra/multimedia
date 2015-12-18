c211d8ad648d5e2989897ccd02f093a0614fc389
Esteban Novo <novo.esteban@gmail.com>
2015-12-18 19:34:47 -0200

    Comenzando con la instalación de los bundles de Media y sus dependencias

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,17 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 	    new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
+            
+            //Sonata Project
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            
+             // Add your dependencies
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            //new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            // Then add SonataAdminBundle
+            //new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
