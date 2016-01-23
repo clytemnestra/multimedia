456bc6a5f0c86df60bb15fbb5e8a68c15f8e73af
Esteban Novo <novo.esteban@gmail.com>
2016-01-23 19:24:55 -0300

    Se generan nuevos campos para las imagenes, filtros y las traducciones

--- src/Application/Sonata/MediaBundle/Admin/MediaAdmin.php
+++ src/Application/Sonata/MediaBundle/Admin/MediaAdmin.php
@@ -2,10 +2,10 @@
 namespace Application\Sonata\MediaBundle\Admin;
 
 use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\MediaBundle\Admin\BaseMediaAdmin as BaseMediaAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\MediaBundle\Admin\ORM\MediaAdmin as BaseMediaAdmin;
 
 class MediaAdmin extends BaseMediaAdmin {
-    
     
     /**
      * {@inheritdoc}
@@ -14,10 +14,44 @@ class MediaAdmin extends BaseMediaAdmin {
     {
         $listMapper
             ->addIdentifier('name')
-            ->add('description')
             ->add('keywords')
+            ->add('description')
             ->add('enabled')
             ->add('size')
         ;
+    }
+    
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('title')
+            ->add('subtitle')
+            ->add('location')
+            ->add('keywords')
+            ->add('providerReference')
+            ->add('enabled')
+            ->add('context')
+        ;
+
+        $providers = array();
+
+        $providerNames = (array) $this->pool->getProviderNamesByContext($this->getPersistentParameter('context', $this->pool->getDefaultContext()));
+        foreach ($providerNames as $name) {
+            $providers[$name] = $name;
+        }
+
+        $datagridMapper->add('providerName', 'doctrine_orm_choice', array(
+            'field_options' => array(
+                'choices'  => $providers,
+                'required' => false,
+                'multiple' => false,
+                'expanded' => false,
+            ),
+            'field_type' => 'choice',
+        ));
     }
 }
