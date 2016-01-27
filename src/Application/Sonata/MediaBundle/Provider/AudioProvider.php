38537bff47b03f1a6060da129d5487e239b0badd
Esteban Novo <novo.esteban@gmail.com>
2016-01-27 12:31:32 -0300

    Trabajo con nuevo provider de audio

--- src/Application/Sonata/MediaBundle/Provider/AudioProvider.php
+++ src/Application/Sonata/MediaBundle/Provider/AudioProvider.php
@@ -1,15 +1,22 @@
 <?php
 namespace Application\Sonata\MediaBundle\Provider;
 
-use Sonata\MediaBundle\Provider\FileProvider;
+use Gaufrette\Filesystem;
+use Imagine\Image\ImagineInterface;
+use Sonata\CoreBundle\Model\Metadata;
+use Sonata\MediaBundle\CDN\CDNInterface;
+use Sonata\MediaBundle\Generator\GeneratorInterface;
+use Sonata\MediaBundle\Metadata\MetadataBuilderInterface;
+use Sonata\MediaBundle\Model\MediaInterface;
+use Sonata\MediaBundle\Thumbnail\ThumbnailInterface;
 
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\MediaBundle\Model\MediaInterface;
 use Symfony\Component\Form\Form;
 
 
 class AudioProvider extends FileProvider
 {
+    protected $imagineAdapter;
     /**
      * @param string                                                $name
      * @param \Gaufrette\Filesystem                                 $filesystem
@@ -20,9 +27,10 @@ class AudioProvider extends FileProvider
      * @param array                                                 $allowedMimeTypes
      * @param \Sonata\MediaBundle\Metadata\MetadataBuilderInterface $metadata
      */
-    public function __construct($name, Filesystem $filesystem, CDNInterface $cdn, GeneratorInterface $pathGenerator, ThumbnailInterface $thumbnail, array $allowedExtensions = array(), array $allowedMimeTypes = array(), MetadataBuilderInterface $metadata = null)
+    public function __construct($name, Filesystem $filesystem, CDNInterface $cdn, GeneratorInterface $pathGenerator, ThumbnailInterface $thumbnail, array $allowedExtensions = array(), array $allowedMimeTypes = array(), ImagineInterface $adapter, MetadataBuilderInterface $metadata = null)
     {
         parent::__construct($name, $filesystem, $cdn, $pathGenerator, $thumbnail, $allowedExtensions, $allowedMimeTypes, $metadata);
+        $this->imagineAdapter = $adapter;
     }
     
     /**
@@ -58,4 +66,140 @@ class AudioProvider extends FileProvider
         $formMapper->add('binaryContent', 'file', array('required' => false));
     }
     
+    public function getMetadata(MediaInterface $media)
+    {
+        if (!$media->getBinaryContent()) {
+
+            return;
+        }
+        
+        $metadata = array(
+            'title' => '',
+            'description' => '',
+            'author_name' => '',
+            'height' => '',
+            'width' => '',
+            'duration' => '',
+            'title' => '',
+        );       
+
+        return $metadata;
+    }
+    
+    public function prePersist(MediaInterface $media)
+    {
+        if (!$media->getBinaryContent()) {
+
+            return;
+        }
+
+        // retrieve metadata
+        $metadata = $this->getMetadata($media);
+
+        // store provider information
+        $media->setProviderName($this->name);
+        $media->setProviderReference($media->getBinaryContent());
+        $media->setProviderMetadata($metadata);
+
+        // update Media common field from metadata
+        $media->setName($metadata['title']);
+        $media->setDescription($metadata['description']);
+        $media->setAuthorName($metadata['author_name']);
+        $media->setHeight($metadata['height']);
+        $media->setWidth($metadata['width']);
+        $media->setLength($metadata['duration']);
+        $media->setContentType('video/x-flv');
+        $media->setProviderStatus(Media::STATUS_OK);
+
+        $media->setCreatedAt(new \Datetime());
+        $media->setUpdatedAt(new \Datetime());
+    }
+    
+    public function preUpdate(MediaInterface $media)
+    {
+        if (!$media->getBinaryContent()) {
+
+            return;
+        }
+
+        $metadata = $this->getMetadata($media);
+
+        $media->setProviderReference($media->getBinaryContent());
+        $media->setProviderMetadata($metadata);
+        $media->setHeight($metadata['height']);
+        $media->setWidth($metadata['width']);
+        $media->setProviderStatus(Media::STATUS_OK);
+
+        $media->setUpdatedAt(new \Datetime());
+    }
+    
+    public function postUpdate(MediaInterface $media)
+    {
+        $this->postPersist($media);
+    }
+
+    public function postPersist(MediaInterface $media)
+    {
+        if (!$media->getBinaryContent()) {
+
+            return;
+        }
+
+        $this->generateThumbnails($media);
+    }
+    
+    public function getReferenceImage(MediaInterface $media)
+    {
+        return $media->getMetadataValue('thumbnail_url');
+    }
+    /*
+    public function getHelperProperties(Media $media, $format, $options = array())
+    {
+        // documentation : http://vimeo.com/api/docs/moogaloop
+        $defaults = array(
+            // (optional) Flash Player version of app. Defaults to 9 .NEW!
+            // 10 - New Moogaloop. 9 - Old Moogaloop without newest features.
+            'fp_version'      => 10,
+
+            // (optional) Enable fullscreen capability. Defaults to true.
+            'fullscreen' => true,
+
+            // (optional) Show the byline on the video. Defaults to true.
+            'title' => true,
+
+            // (optional) Show the title on the video. Defaults to true.
+            'byline' => 0,
+
+            // (optional) Show the user's portrait on the video. Defaults to true.
+            'portrait' => true,
+
+            // (optional) Specify the color of the video controls.
+            'color' => null,
+
+            // (optional) Set to 1 to disable HD.
+            'hd_off' => 0,
+
+            // Set to 1 to enable the Javascript API.
+            'js_api' => null,
+
+            // (optional) JS function called when the player loads. Defaults to vimeo_player_loaded.
+            'js_onLoad' => 0,
+
+            // Unique id that is passed into all player events as the ending parameter.
+            'js_swf_id' => uniqid('vimeo_player_'),
+        );
+
+        $player_parameters =  array_merge($defaults, isset($options['player_parameters']) ? $options['player_parameters'] : array());
+
+        $params = array(
+            'src'         => http_build_query($player_parameters),
+            'id'          => $player_parameters['js_swf_id'],
+            'frameborder' => isset($options['frameborder']) ? $options['frameborder'] : 0,
+            'width'       => isset($options['width'])             ? $options['width']  : $media->getWidth(),
+            'height'      => isset($options['height'])            ? $options['height'] : $media->getHeight(),
+        );
+
+        return $params;
+    }
+    */
 }
